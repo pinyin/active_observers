1deb99ae3e6d2bb3567f3d6ffada58f5a9046c73
pinyin <bob432091@gmail.com>
2019-03-31 23:54:12 +0800

    update document

--- lib/src/observe_paint.dart
+++ lib/src/observe_paint.dart
@@ -2,6 +2,12 @@ import 'package:flutter/widgets.dart';
 
 import 'observe_lifecycle.dart';
 
+/// Run [callback] after initial paint, then call it again when this state
+/// is updated and [rerunWhen] returns true.
+/// Since [callback] is called after paint, it can read geometric information in
+/// current and descendant widgets.
+/// Notice: this observer will not be triggered by paints caused by parent
+/// widgets, e.g. constraint updates from parent widget.
 void observePaint(void callback(), {bool Function() rerunWhen}) {
   var hasScheduledTask = false;
 
