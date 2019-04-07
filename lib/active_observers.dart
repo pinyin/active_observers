c2d9c72137f31ee52e35cb42100af2bc2eb7ae1a
pinyin <bob432091@gmail.com>
2019-04-07 22:49:13 +0800

    export Memo

--- lib/active_observers.dart
+++ lib/active_observers.dart
@@ -1,9 +1,11 @@
 library active_observers;
 
 export 'src/active_observers.dart' show ActiveObservers, StateLifecyclePhase;
+export 'src/observe_dispose.dart' show observeDispose;
 export 'src/observe_effect.dart' show observeEffect;
 export 'src/observe_lifecycle.dart' show observeLifecycle;
 export 'src/observe_listenable.dart' show observeListenable;
 export 'src/observe_memo.dart' show observeMemo;
 export 'src/observe_paint.dart' show observePaint;
 export 'src/observe_stream.dart' show observeStream;
+export 'src/utils.dart' show Memo;
