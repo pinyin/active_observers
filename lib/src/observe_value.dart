9f7c548143a847f367bd943a2877d986aad42777
pinyin <bob432091@gmail.com>
2019-04-08 14:35:07 +0800

    always recompute value by default

--- lib/src/observe_value.dart
+++ lib/src/observe_value.dart
@@ -4,9 +4,14 @@ import './utils.dart';
 Memo<T> observeValue<T>(T compute(), {bool recomputeWhen(), Iterable deps()}) {
   final notifier = MemoController<T>(null);
 
+  recomputeWhen =
+      recomputeWhen == null && deps == null ? () => true : recomputeWhen;
+
   observeEffect(
     () {
-      notifier.value = compute();
+      final currentValue = compute();
+      if (currentValue == notifier.value) return;
+      notifier.value = currentValue;
     },
     restartWhen: recomputeWhen,
     deps: deps,
