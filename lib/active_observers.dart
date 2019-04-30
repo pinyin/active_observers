140250f11bbbb765953acef9e3c2ce3ee2e017dd
pinyin <bob432091@gmail.com>
2019-04-30 15:16:13 +0800

    export DetailedLifecycle

--- lib/active_observers.dart
+++ lib/active_observers.dart
@@ -1,7 +1,11 @@
 library active_observers;
 
 export 'src/active_observers.dart'
-    show ActiveObservers, StateLifecyclePhase, DetailedLifecycleInState;
+    show
+        ActiveObservers,
+        StateLifecyclePhase,
+        DetailedLifecycleInState,
+        DetailedLifecycle;
 export 'src/observe_context.dart' show observeContext;
 export 'src/observe_dispose.dart' show observeDispose;
 export 'src/observe_effect.dart' show observeEffect;
